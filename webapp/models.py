feaa3f70973321d4c80079ab5e6ae5f04f218ba2
TumashenkaAliaksandr <Badminton500@inbox.lv>
2024-03-19 12:50:47 +0300

    added meta name fields

--- webapp/models.py
+++ webapp/models.py
@@ -14,3 +14,7 @@ class Doctor(models.Model):
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
+    class Meta:
+        verbose_name = "Доктора"
+        verbose_name_plural = "Доктора"
+
