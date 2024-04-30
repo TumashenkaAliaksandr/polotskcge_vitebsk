355cda41012e464cdbf4d3d71c91bc804790e3e9
TumashenkaAliaksandr <Badminton500@inbox.lv>
2024-04-30 15:17:16 +0300

    fixing settings

--- polotskcge_vitebsk/settings.py
+++ polotskcge_vitebsk/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8^eoeh7ubz!x5@6v#(4g!(2qd*fi$0s94w!j1mr_=bm6v@055j'
 
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
