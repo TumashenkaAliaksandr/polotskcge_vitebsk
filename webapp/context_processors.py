cda9348c17136c1a950d8d0e6476a79dba12ee44
Phoenix Pegasus <tumashenkaaliaksandr@gmail.com>
2025-05-13 09:19:01 +0300

    deleted not using file gettext-setup.exe and translated what you need into Belarusian and Russian.

--- webapp/context_processors.py
+++ webapp/context_processors.py
@@ -2,7 +2,7 @@ from .models import ContactInfoHad, Logo, Contacts
 
 
 def common_context(request):
-    info_contact = ContactInfoHad.objects.first()  # Возьмёт первый объект или None
+    info_contact = ContactInfoHad.objects.first()
     logo_main = Logo.objects.first()
     contacts_footer = Contacts.objects.first()
     return {
