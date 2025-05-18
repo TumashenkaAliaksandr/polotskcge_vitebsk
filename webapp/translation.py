b060e8231925971f299479c45ef8e56553775e29
Phoenix Pegasus <tumashenkaaliaksandr@gmail.com>
2025-05-18 14:13:55 +0300

    new styles mobile

--- webapp/translation.py
+++
@@ -1,7 +0,0 @@
-# site_app/translation.py
-from modeltranslation.translator import register, TranslationOptions
-from .models import AboutHistory
-#
-# @register(AboutHistory)
-# class AboutHistoryTranslationOptions(TranslationOptions):
-#     fields = ('name', 'description')
